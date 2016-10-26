c132b622d52a28c2764eca550ac39e1b8c3d6690
Katona Mátyás <matyesz5@freestart.hu>
2016-10-26 11:04:03 +0200

    Index
    na csa

---
+++ index.php
@@ -0,0 +1,24 @@
+//Get Shrekt
+<html>
+	<head> </head>
+	
+	<body>
+		
+	
+	
+	</body>
+	
+		  <form action="" method="post">
+		  <input type="text" name="username" />
+		  <input type="password" name="password" />
+		  <input type="submit" name="submit" />
+			
+	  </form>
+
+
+</html>
+
+
+<?php
+
+?>
