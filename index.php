d09b26bbb97e0fe82a1a432cfc3cd8005912af86
HapasiL <laci.havasi13@gmail.com>
2016-10-26 11:15:43 +0200

    ok
    javítva

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 //Get Shrekt
 <html>
-	<head> </head>
+	<head>
+		<title>Shrekbook™</title>
+	</head>
 	
 	<body>
-		
-	
-	
+		<form action="" method="post">
+			<input type="text" name="username" />
+			<input type="password" name="password" />
+			<input type="submit" name="submit" />
+		</form>
+
 	</body>
 	
-		  <form action="" method="post">
-		  <input type="text" name="username" />
-		  <input type="password" name="password" />
-		  <input type="submit" name="submit" />
-			
-	  </form>
-
 
 </html>
 
