1f443f555424e6b89728724fd93b4c92681ab1df
Katona Mátyás <matyesz5@freestart.hu>
2016-10-26 13:59:44 +0200

    Reg

---
+++ reg.php
@@ -0,0 +1,25 @@
+
+//Get Shrekt
+<html>
+	<head>
+		<title>Shrekbook™</title>
+	</head>
+	
+	<body>
+		<form action="" method="POST">
+		<div class="center">
+			<div class="spacing"><label>E-mail: </label><input type="text" name="email"></div>
+			<div class="spacing"><label>User name: </label><input type="text" name="username"></div>
+			<div class="spacing"><label>Password: </label><input type="password" name="password"></div>
+			
+			<form enctype="multipart/form-data" action="" method="POST">				
+				<div class="spacing"><input type="file" name="fajl"></div>
+			</form>
+				<div class="spacing"><input type="submit" name="Register" value="Register"></div>
+		</div>
+	</form>
+
+	</body>
+	
+
+</html>
