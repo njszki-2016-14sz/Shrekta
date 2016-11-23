204c91fb20a8a35dff05ee838ac797228ca07d72
miliczkib <miliczki.balazs01@gmail.com>
2016-11-23 12:03:54 +0100

    faszapicsádba

---
+++ shrekbook/shrekbook/database.php
@@ -0,0 +1,18 @@
+<?php 
+	$link = mysql_connect('localhost', 'root', '');
+		if (!$link) {
+			die('Not connected : ' . mysql_error());
+		}
+
+	
+	$db_selected = mysql_select_db('shrekbook', $link);
+		if (!$db_selected) {
+			die ('Can\'t connect : ' . mysql_error());
+		}
+	
+	
+	
+	
+
+
+?>
