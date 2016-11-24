7fca1749c7101c5b96a604d766fe4864617da224
miliczkib <miliczki.balazs01@gmail.com>
2016-11-24 11:52:10 +0100

    post

--- NewLoginReg/index.php
+++ NewLoginReg/index.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 include_once 'dbconnect.php';
+include_once 'post.php';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home | Koding Made Simple</title>
+    <title>Shrekbook</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" >
     <link href="shrek.css" type="text/css" rel="stylesheet" />
     <link rel="shortcut icon" href="http://www.iconarchive.com/download/i61338/majdi-khawaja/shrek/Shrek.ico" type="favicon/ico" />
@@ -29,6 +30,62 @@ include_once 'dbconnect.php';
         </div>
     </div>
 </nav>
+	<!---------------------------------------------------------------------------------------------------------------------->
+	<?php if(!empty($_SESSION['usr_name'])) { ?>
+		<div class="newpost">
+			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="uppost">
+				<div class="form-group">
+                    <label for="name">Text</label>
+					
+					<span class="text-danger"><?php if (isset($text_error)) echo $text_error; ?></span>
+                </div>
+			
+		
+			</div>
+			
+				<div id="post-editor" class="post-editor js-post-editor">
 
+					<div style="position: relative;">     
+						<div class="wmd-container">
+							<div id="wmd-button-bar" class="wmd-button-bar"></div>
+							<textarea id="wmd-input" class="wmd-input" name="post-text" cols="92" rows="15" tabindex="101" data-min-length=""></textarea>
+						</div>
+					</div>
+
+					<div class="fl" style="margin-top: 8px; height:24px;">&nbsp;</div>
+					<div id="draft-saved" class="draft-saved community-option fl" style="margin-top: 8px; height:24px; display:none;">draft saved</div>
+
+					<div id="draft-discarded" class="draft-discarded community-option fl" style="margin-top: 8px; height:24px; display:none;">draft discarded</div>
+					
+					<input type="submit" name="UpPost" value="Shrek Up!">
+
+
+
+
+
+				</div>
+			</div>
+			<?php
+			
+				$error = false;
+					if(isset($_POST['UpPost'])){
+						if(empty($_POST['post-text'])){
+							$text_error = "Nem lehet üresen hagyni";
+							$error = true;
+						}
+						if(!$error) Post::NewPost($_SESSION['usr_name'], $_POST['post-text'], $con);
+					}
+
+				?>
+
+			<div class="listpost">
+
+				<?php
+					Post::ListPost($con);
+			}
+				?>
+		</div>
+		
+		<!--------------------------------------------------------------------------------------------------------------------------------------->
 </body>
 </html>
