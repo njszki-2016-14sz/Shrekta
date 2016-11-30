615de4ab32868d8ce63033176e98549279f587a9
Katona Mátyás <matyesz5@freestart.hu>
2016-11-30 12:18:23 +0100

    slide Pék

--- NewLoginReg/login.php
+++ NewLoginReg/login.php
@@ -30,6 +30,8 @@ if (isset($_POST['login'])) {
     <title>Shrekbook</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" >
     <link href="shrek.css" type="text/css" rel="stylesheet" />
+      <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="shortcut icon" href="http://www.iconarchive.com/download/i61338/majdi-khawaja/shrek/Shrek.ico" type="favicon/ico" />
 </head>
 <body>
@@ -102,7 +104,13 @@ if (isset($_POST['login'])) {
   </form>
 
 </div>
+    <div> <img id="pics"  src="shrek.png" > </div>
 
+        <script>
+        $( document ).ready(function() {
+          $( "div" ).effect( "slide", "slow" );
+        });
+        </script>
 
 </body>
 </html>
