e8771aab9552d22cee5c5bf77d2923a9b5f2fd33
miliczkib <miliczki.balazs01@gmail.com>
2017-01-19 12:00:33 +0100

    jó

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 $db_username = 'root';
 $db_password = '';
-$db_name = 'products';
+$db_name = 'shrek';
 $db_host = 'localhost';
 	
 							
--- php/index.php
+++ php/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include_once("config.php");
+include 'funct.php';
 
 
 $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
@@ -11,16 +12,162 @@ $current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Shopping Cart</title>
 <link href="style/style.css" rel="stylesheet" type="text/css">
+<link href="style/shrek.css" rel="stylesheet" type="text/css">
 </head>
 <body>
   <audio controls autoplay="autoplay">
   <source src="Allstar.mp3" type="audio/mpeg" preload="auto">
     <source src="Allstar.ogg" type="audio/ogg" >
   <p>Your browser does not support the audio element.</p>
-</audio> 
+</audio>
+
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        
+        </div>
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="nav navbar-nav navbar-right">
+                <?php if (isset($_SESSION['usr_id'])) { ?>
+                <li><a href="index.php?action=index">Signed in as <?php echo $_SESSION['usr_name']; ?></a></li>
+					
+				<li><a href="index.php?action=shop"><img src="shop.jpg"> </a> </li> 
+                <li><a href="index.php?action=logout">Shrek Out</a></li>
+                <?php 
+					} else { ?>
+                <li><a href="index.php?action=login">Shrek in</a></li>
+                <li><a href="index.php?action=register">Shrek Up</a></li>
+				<li><a href="index.php?action=shop"><img src="shop.jpg"> </a> </li>
+                <?php }
+				?>
+            </ul>
+        </div>
+    </div>
+</nav> 
+
+			<?php 
+				if(isset($_GET['action'])) {
+							switch ($_GET['action'])
+							{	
+										case 'logout': User::LogOut(); break;
+							}
+				}
+			?>
+
+<!--- LOGIN - REGISTER !! ------------------------------------------------------------------------------------------------------------------------------------>
+		
+		<?php  if(empty($_SESSION['usr_name'])) { 
+		if(isset($_GET['action'])) {
+					switch ($_GET['action'])
+					{	
+						case 'login': /*login*/
+						
+						if(isset($_SESSION['usr_id'])!="") {
+							header("Location: index.php");
+						}
+							//$errormsg = "";
+						
+						if (isset($_POST['login'])) {	
+							User::Login($_POST['email'], $_POST['password'], $con);
+						}
+		?>
+				
+					<div id="wrapper">
+
+					  <form name="login-form" class="login-form" action="" method="post">
+					  
+						<div class="header">
+						<h1>Shrekbook</h1>
+						<span>What are you doing in my swamp?</span>
+						<?php
+						if(isset($_SESSION["error"]))
+									{
+										$error=$_SESSION["error"];?>
+										<br><span id='hint'><?php print $error;?></span><br><?php
+										$_SESSION["error"]=null;
+									}
+						?>
+						</div>
+					  
+						<div class="content">
+						<input name="email" type="text" class="input username" required class="form-control" placeholder="Email" />
+						<div class="user-icon"></div>
+						<input name="password" type="password" class="input password" placeholder="Password" required class="form-control" />
+						<div class="pass-icon"></div>   
+						</div>
+						<div class="footer">
+						<input type="submit" name="login" value="Shrek In" class="button" class="btn btn-primary" />
+					  
+						</div>
+					  
+					  </form>
+
+					</div>
+					<div> <img id="pics"  src="shrek.png" > 
+					</div>
+
+							<script>
+							$( document ).ready(function() {
+							  $( "div" ).effect( "slide", "slow" );
+							});
+							</script>
+				<?php 
+						; break;
+				
+						case 'register': /*register*/
+						
+						if(isset($_SESSION['usr_id'])) {
+							header("Location: index.php");
+						}
+						$error = false;
+						if (isset($_POST['signup'])) {
+						   User::Register($con, $_POST['name'], $_POST['email'], $_POST['password'], $_POST['cpassword'], $error);
+
+						}
+				?>
+						<div id="wrapper">
+
+						  <form name="login-form" class="login-form" action="" method="post">
+						  
+							<div class="header">
+							<h1>Shrekbook</h1>
+							<span>Only for green ones.</span>
+							<?php
+										if(isset($_SESSION["error"]))
+										{
+											$error=$_SESSION["error"];?>
+											<span id='hint'><?php print $error;?></span><br><?php
+											$_SESSION["error"]=null;
+										}
+							?>
+							</div>
+						  
+							<div class="content">
+								<input name="name" type="text" class="input username" required class="form-control" placeholder="Enter Full Name" />
+								
+								<input name="email" type="text" class="input password" class="form-control" placeholder="Email" required class="form-control"  />
+							   							
+								<input name="password" type="password" class="input password" class="form-control" placeholder="Password" required class="form-control" />
+
+								<input name="cpassword" type="password" class="input password" class="form-control" placeholder="Confirm Password" required class="form-control" />
+								
+							</div>
+							
+							<div class="footer">
+							<input type="submit" name="signup" value="Shrek Up" class="button" class="btn btn-primary" />					  
+							</div>					  
+						  </form>
+						</div>
+							</div>
+						</div>
+					
+						<?php						
+						; break;
+					}
+		}	
+		} else {
+				?>
+		<!-- LOG/REG Vége -->	
 <h1 align="center">Shrek Shop (Better than Ebay) </h1>
-
-
 <?php
 if(isset($_SESSION["cart_products"]) && count($_SESSION["cart_products"])>0)
 {
@@ -101,6 +248,7 @@ EOT;
 $products_item .= '</ul>';
 echo $products_item;
 }
+		}
 ?>    
 
 </body>
